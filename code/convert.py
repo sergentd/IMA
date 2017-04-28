2439d8255849d23edbdb558c833ef3645082269c
enzopoggio <poggio.enzo@gmail.com>
2017-04-28 20:49:53 +0200

    modifié :         code/convert.py

--- code/convert.py
+++ code/convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This script permit to convert videos into matrix which all of same size
+This script permit to convert videos into matrix which
 of Openface's Action Units (AU).
 """
 import argparse
@@ -12,7 +12,7 @@ import numpy as np
 
 def parse():
     parser = argparse.ArgumentParser(description="""
-        Convert videos into matrix which all of same size
+        Convert videos into matrix which
         of Openface's Action Units.
         """)
     parser.add_argument(
@@ -20,23 +20,27 @@ def parse():
         metavar='DIR/VID',
         type=dir,
         default="../data/vid_usr",
-        help='file where you generate your videos with showVid.ipynb')
+        help='dir where you generate your videos with showVid.ipynb')
     parser.add_argument(
         '-t', '-target',
-        metavar='DIR/VID',
+        metavar='DIR/AU',
         type=dir,
-        default="../data/vid_usr",
-        help='file where you generate your videos with showVid.ipynb')
+        default="../data/au_usr",
+        help='dir where you want to store your AU matrix')
     return parser
 
-def __get_data__():
+
+def __get_data__(source):
+    pass
+
+
+def resize():
     pass
 
 
 def main():
-    parser = parse()
-
-
-
+    args = parse()
+    source, target = args.source, args.target
+    __get_data__(source)
 if __name__ == '__main__':
     main()
