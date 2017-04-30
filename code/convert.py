0e915c069bdfc93af5240a6fd3761e7ca78a74c0
enzopoggio <poggio.enzo@gmail.com>
2017-04-30 18:31:40 +0200

    modifié :         convert.py 	nouveau fichier : ../data/vid_usr/id1/idVidYt.txt 	nouveau fichier : ../data/vid_usr/id2/idVidYt.mp4 	nouveau fichier : ../data/vid_usr/id2/idVidYt.txt

--- code/convert.py
+++ code/convert.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict as dd
 
 
 def parse():
@@ -16,22 +17,35 @@ def parse():
         of Openface's Action Units.
         """)
     parser.add_argument(
-        '-s', '-source',
+        '-s',
         metavar='DIR/VID',
-        type=dir,
+        type=str,
         default="../data/vid_usr",
         help='dir where you generate your videos with showVid.ipynb')
     parser.add_argument(
-        '-t', '-target',
+        '-t',
         metavar='DIR/AU',
-        type=dir,
-        default="../data/au_usr",
+        type=str,
+        default="../data/au_matrix_usr",
         help='dir where you want to store your AU matrix')
-    return parser
+    return parser.parse_args()
 
 
-def __get_data__(source):
-    pass
+def get_data_vid(source):
+    """ Walk files into data to convert each videos """
+    def useOpenface(save_point, vid_yt , ref_vid):
+
+        os.system("ls ../../../poggioe0/OpenFace/build/bin/FeatureExtraction")
+        command = "./../../OpenFace/build/bin/FeatureExtraction -f "
+        command += vid_yt
+        command += " -of "+ref_vid+".txt -root "+save_point
+        os.system(command)
+    dict_matrix = dd(lambda: dd(list))
+    for path_id_usr in os.listdir(source):
+        for vid_yt in os.listdir(source+"/"+path_id_usr):
+            ref_vid = vid_yt[:-4]
+            useOpenface(source+"/"+path_id_usr, vid_yt, ref_vid)
+            dict_matrix[path_id_usr][ref_vid] = [0, 0, 0, 0]
 
 
 def resize():
@@ -40,7 +54,10 @@ def resize():
 
 def main():
     args = parse()
-    source, target = args.source, args.target
-    __get_data__(source)
+    source, target = args.s, args.t
+    # print(source)
+    get_data_vid(source)
+
+
 if __name__ == '__main__':
     main()
