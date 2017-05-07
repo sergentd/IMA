27917031b6f530cc9106cf8614bedf1fb130f930
Djavan <djavan.sergent@gmail.com>
2017-05-07 14:58:30 +0200

    ajout params et evaluator

--- scripts/interface/interface.py
+++ scripts/interface/interface.py
@@ -1,30 +1,36 @@
+#! /usr/bin/python3
 import tkinter as tk
-from PIL import Image, ImageTk
 
 
-class Interface():
+class Interface:
+    """
+    A class which display evaluation tools
+    @Djavan Sergent
+    """
 
-    def __init__(self, title="MakeMeLaught", bg="#FFFFFF", sizex=400, sizey=100):
+    def __init__(self, video, params, evaluator, title="MakeMeLaught", bg="#FFFFFF", sizex=400, sizey=100):
         self.window = tk.Tk()
+        self.par = params
         self.title = title
         self.background = bg
         self.size_x = sizex
         self.size_y = sizey
-        self.ctrl_frame = tk.Frame(self.window, width=self.size_x, height=self.size_y)
+        self.video = video
+        self.ev = evaluator
 
     def create(self):
         self.window.wm_title(self.title)
         self.window.config(background=self.background)
-        btn_next = tk.Button(self.window, text="Ok", command=self.cmd_next)
-        slider = tk.Scale(self.window, from_=0, to=10, length=400, tickinterval=1, orient=tk.HORIZONTAL)
-        slider.pack()
-        btn_next.pack()
+        self.btn_next = tk.Button(self.window, text="Ok", command=self.cmd_eval)
+        self.slider = tk.Scale(self.window, from_=0, to=10, length=self.size_x, tickinterval=1, orient=tk.HORIZONTAL)
+        self.slider.pack()
+        self.btn_next.pack()
 
-    def cmd_next(self):
-        print("next")
-
-    def cmd_play(self):
-        print("play")
+    def cmd_eval(self):
+        evaluation = self.par.user + ";" + self.video + ";" + str(self.slider.get())
+        self.ev.add(evaluation)
+        self.window.quit()
+        self.window.destroy()
 
     def show(self):
         self.window.mainloop()
