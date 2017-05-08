454a612002d89dc230b7e31a5f9b55e818cdf11b
Djavan <djavan.sergent@gmail.com>
2017-05-08 12:01:09 +0200

    database manager

---
+++ scripts/database/dbmanager.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+# @Djavan Sergent
+# See http://docs.sqlalchemy.org/en/latest/orm/tutorial.html for more details about SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+# Database engine and session parameters
+Base = declarative_base()
+engine = create_engine('sqlite:///../../data/ima.db', echo=False)  # echo = logging in console
+Session = sessionmaker(bind=engine)
+
+# Classes which needed a Base, no on top import
+from video import Video
+from user import User
+from videoevallink import VideoUserLink  # Used for table creation ! do not delete this line !
