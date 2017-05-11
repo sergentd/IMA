fd8ef22a50e492196356be38ec7b0f8bd49880c3
Djavan <djavan.sergent@gmail.com>
2017-05-11 16:23:27 +0200

    center function

--- scripts/function.py
+++ scripts/function.py
@@ -25,3 +25,19 @@ def list(dir):
 
 def afpath(video):
     return splitext(basename(video))[0] + ".wav"
+
+
+def center(window):
+    w = window.winfo_width()
+    h = window.winfo_height()
+
+    ws = window.winfo_screenwidth()  # width of the screen
+    hs = window.winfo_screenheight()  # height of the screen
+
+    # calculate x and y coordinates for the Tk root window
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+
+    # set the dimensions of the screen
+    # and where it is placed
+    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
