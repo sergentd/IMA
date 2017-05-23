3c07f55b28c63cb5b62fecf2e943c923e81f6d1c
Djavan <djavan.sergent@gmail.com>
2017-05-23 15:08:04 +0200

    cleanup

--- scripts/function.py
+++ scripts/function.py
@@ -3,6 +3,7 @@
 from os import listdir, makedirs
 from os.path import isfile, join, exists, basename, splitext
 from interface.views import ChooseMediaPlayerView
+from matplotlib import pyplot as plt
 
 
 # check the existence of a directory
@@ -23,25 +24,22 @@ def list(dir):
     return [f for f in listdir(dir) if isfile(join(dir, f))]
 
 
-def afpath(video):
-    return splitext(basename(video))[0] + ".wav"
-
-
-def center(window):
-    w = window.winfo_width()
-    h = window.winfo_height()
-
-    ws = window.winfo_screenwidth()  # width of the screen
-    hs = window.winfo_screenheight()  # height of the screen
-
-    # calculate x and y coordinates for the Tk root window
-    x = (ws / 2) - (w / 2)
-    y = (hs / 2) - (h / 2)
-
-    # set the dimensions of the screen
-    # and where it is placed
-    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-
+# filename without extension
 def get_filename(file):
     return splitext(basename(file))[0]
+
+
+# Plot the dummy analyse
+def plot(name, label, au, auv):
+    # Plot title
+    plt.suptitle(name + " : " + label)
+    # Subplot 1 : AU presence
+    plt.subplot(211)
+    for e in au:
+        plt.plot(e)
+    # Subplot 2 : AU intensity
+    plt.subplot(212)
+    for e in auv:
+        plt.plot(e)
+
+    plt.show()
