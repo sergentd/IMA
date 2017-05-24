a8c49c35b94734df1e43ce8fc2128104d92c406b
enzopoggio <poggio.enzo@gmail.com>
2017-05-24 12:51:06 +0200

    TODO fix recall and precision add fscore

--- scripts/utils/mesure.py
+++ scripts/utils/mesure.py
@@ -15,29 +15,29 @@ def create_dic(pred_label):
 
 
 def conf_tab(label_pred):
-    y_label = pd.Series([y for y, _ in label_pred], name='Actual')
-    y_pred = pd.Series([y for _, y in label_pred], name='Predicted')
-    df_confusion = pd.crosstab(y_label, y_pred, rownames=['Actual'], colnames=[
-                               'Predicted'], margins=True)
+    y_label = pd.Series([y for _, y in label_pred], name='Actual')
+    y_pred = pd.Series([y for y, _ in label_pred], name='Predicted')
+    df_confusion = pd.crosstab(y_label, y_pred, rownames=['R:Actual'], colnames=[
+                               'C:Predicted'], margins=True)
     return str(df_confusion)
 
 
 def accuracy(label_pred):
-    y_label = [y for y, _ in label_pred]
-    y_pred = [y for _, y in label_pred]
+    y_label = [y for _, y in label_pred]
+    y_pred = [y for y, _ in label_pred]
     return "Accuracy:\t{:.0%}".format(accuracy_score(y_label, y_pred))
 
 
 def precision(label_pred):
-    y_label = [y for y, _ in label_pred]
-    y_pred = [y for _, y in label_pred]
+    y_label = [y for _, y in label_pred]
+    y_pred = [y for y, _ in label_pred]
     return "Precision:\t{:.0%}".format(
         precision_score(y_label, y_pred, average='micro'))
 
 
 def recall(label_pred):
-    y_label = [y for y, _ in label_pred]
-    y_pred = [y for _, y in label_pred]
+    y_label = [y for _, y in label_pred]
+    y_pred = [y for y, _ in label_pred]
     return "Recall:\t\t{:.0%}".format(
         recall_score(y_label, y_pred, average='micro'))
 
@@ -49,15 +49,16 @@ def error_grade(label_pred):
 
 def all_mesure_grade(label_pred):
     return "\n".join([
-        conf_tab(label_pred), error_grade(label_pred)])
+        conf_tab(label_pred), error_grade(label_pred), ""])
 
 
 def all_mesure_funny(label_pred):
     return "\n".join([
         conf_tab(label_pred),
         accuracy(label_pred),
-        precision(label_pred),
-        recall(label_pred)])
+        # precision(label_pred),
+        # recall(label_pred),
+        ""])
 
 
 def main():
