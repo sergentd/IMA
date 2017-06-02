ccc1fb5c070e3c4df3bc0510e298d988ca30af38
Djavan <djavan.sergent@gmail.com>
2017-06-02 09:35:18 +0200

    faxp

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -12,17 +12,27 @@ class Predictor:
 
     def launch(self):
         labels = self.get_labels(self.par.eval_path + self.par.user + '.csv')
-        predictions = dict()
+        predictions = []
         for m in self.get_matrix():
             video_name = get_filename(m)
             csv_path = self.au_dir + m
-            predictions[video_name] = [self.analyze(csv_path, labels[video_name], video_name), labels[video_name]]
+            predictions.append([self.analyze(csv_path, labels[video_name], video_name), labels[video_name]])
 
         # for p in predictions:
         #     print(p + str(predictions[p]))
 
         truePos, trueNeg, falsePos, falseNeg = self.scoring(predictions)
         return {'tp':truePos, 'tn':trueNeg, 'fp':falsePos, 'fn':falseNeg}
+
+    def facialExp(self):
+        faxp = self.get_facial_exp(self.par.eval_path + self.par.user + '.csv')
+        predictions = []
+        for m in self.get_matrix():
+            video_name = get_filename(m)
+            csv_path = self.au_dir + m
+            predictions.append([self.eval_facial_expression(csv_path, faxp[video_name]), float(faxp[video_name])])
+        return self.scoring_faxp(predictions)
+
 
     def get_matrix(self):
         return list(self.au_dir)
@@ -35,10 +45,38 @@ class Predictor:
                 labels[get_filename(line['video_name'])] = line['funny']
         return labels
 
+    def get_facial_exp(self, ufile):
+        faxp = dict()
+        with open(ufile, newline='') as csvfile:
+            faxp_reader = csv.DictReader(csvfile)
+            for line in faxp_reader:
+                faxp[get_filename(line['video_name'])] = line['grade']
+        return faxp
+
+
+    # dummy prediction
+    def eval_facial_expression(self, mfile, grade):
+        au = {'AU12_c':0}
+        with open(mfile, newline='') as csvfile:
+            matrix_reader = csv.DictReader(csvfile)
+            cpt=0
+            tot=0
+            for line in matrix_reader:
+                for el in line:
+                    if el in au:
+                        au[el] += float(line[el])
+                        tot+= float(line[el])
+                cpt += 1
+        res = (tot/cpt)*10
+        print(res, " : ", grade)
+        return res
+
+
     # Dummy prediction
     def analyze(self, mfile, label, name):
         au6 = []; au6v = []
         au12 = []; au12v = []
+        au5 = []; au5v = []
         match = 0
 
         with open(mfile, newline='') as csvfile:
@@ -47,28 +85,28 @@ class Predictor:
             for line in matrix_reader:
                 au6.append(line["AU06_c"]); au6v.append(line["AU06_r"])
                 au12.append(line["AU12_c"]); au12v.append(line["AU12_r"])
+                # au5.append(line["AU05_c"]); au5v.append(line["AU05_r"])
                 if (line["AU06_c"] == str(1)) and (line["AU12_c"] == str(1)):
                     match += 1
                 cpt += 1
 
-            # Plot
-            au = [au6, au12]
-            auv = [au6v, au12v]
-            # plot(name, label, au, auv)
+        res = str((match/cpt >= 0.2))
 
-            # Prediction
-            if match/cpt >= 0.2:
-                return 'True'
-            else:
-                return 'False'
+        # Plot
+        au = [au6, au12, au5]
+        auv = [au6v, au12v, au5v]
+        #plot(name, label, res, au, auv)
+
+        # Prediction
+        return res
 
     def scoring(self, predictions):
         true_positif = 0; true_negatif = 0; false_positif = 0; false_negatif = 0
         for p in predictions:
             # Prediction = TRUE
-            if predictions[p][0] == 'True':
+            if p[0] == 'True':
                 # True label = TRUE
-                if predictions[p][1] == 'True':
+                if p[1] == 'True':
                     true_positif += 1
                 # True label = FALSE
                 else:
@@ -76,7 +114,7 @@ class Predictor:
             # Prediction = FALSE
             else:
                 # True label = TRUE
-                if predictions[p][1] == 'True':
+                if p[1] == 'True':
                     false_negatif += 1
                 # True label = FALSE
                 else:
@@ -86,6 +124,16 @@ class Predictor:
         # print("P : FALSE\t|\tT : FALSE\t" + str(true_negatif))
         # print("P : FALSE\t|\tT : TRUE\t" + str(false_negatif))
         return true_positif, true_negatif, false_positif, false_negatif
+
+    def scoring_faxp(self, predictions):
+        avg = 0
+        for p in predictions:
+            avg += abs(p[0] - p[1])
+        if len(predictions)>0:
+            moy = avg/len(predictions)
+        else:
+            moy = 0
+        return moy
 
 
 if __name__ == "__main__":
@@ -98,14 +146,17 @@ if __name__ == "__main__":
     for e in evals:
         list_users.append(get_filename(e))
 
+    results = 0
     for user in list_users:
         par.set_user(user)
         # predictor
         predic = Predictor(params=par)
-        results = predic.launch()
-        preds['tp'] += results['tp']
-        preds['tn'] += results['tn']
-        preds['fp'] += results['fp']
-        preds['fn'] += results['fn']
-    print(preds)
-    print((preds['tp'] + preds['tn'])*100 / (preds['tp'] + preds['tn'] + preds['fp'] + preds['fn']))
+    #     results = predic.launch()
+    #     preds['tp'] += results['tp']
+    #     preds['tn'] += results['tn']
+    #     preds['fp'] += results['fp']
+    #     preds['fn'] += results['fn']
+    # print(preds)
+    # print((preds['tp'] + preds['tn'])*100 / (preds['tp'] + preds['tn'] + preds['fp'] + preds['fn']))
+        results += predic.facialExp()
+    print("*** écart moyen : " , results/len((list_users)), " ***")
